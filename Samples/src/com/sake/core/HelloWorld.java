f866632ecfce0610b51da71f374affec2291ca02
Praveen Sake <praveensake@gmail.com>
2015-08-29 23:45:10 -0400

    changes

--- Samples/src/com/sake/core/HelloWorld.java
+++ Samples/src/com/sake/core/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.print("Hello World");
+        System.out.print("Hello World\n");
         System.out.print("Test 123");
     }
 }
